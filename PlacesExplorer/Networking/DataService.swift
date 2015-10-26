b4840c2d8a74d7c79fdf41df4cf0f0c970b9cf54
Madhu Samuel <madhusamuel@gmail.com>
2015-10-26 12:57:15 +1100

    Added DataService to access web services.

---
+++ PlacesExplorer/Networking/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  PlacesExplorer
+//
+//  Created by Madhu Samuel on 26/10/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    
+    func login(userName: String, password: String, success:()->(), failure: (error: NSError)->()) {
+        print("Loggin in \(userName)")
+    }
+}
