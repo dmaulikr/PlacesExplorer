9fca7d6bc6569e4ab2e628b82c8557a1befaca30
Madhu Samuel <madhusamuel@gmail.com>
2015-10-26 15:21:18 +1100

    Linked venueTableViewCell from storyboard to code.

---
+++ PlacesExplorer/Venues/VenueListTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  VenueListTableViewCell.swift
+//  PlacesExplorer
+//
+//  Created by Madhu Samuel on 26/10/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class VenueListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var venueImageView: UIImageView!
+    @IBOutlet weak var venueTitleLabel: UILabel!
+    @IBOutlet weak var venueTypeLabel: UILabel!
+    @IBOutlet weak var venueExpenseRangeLabel: UILabel!
+    @IBOutlet weak var venueDistanceLabel: UILabel!
+    @IBOutlet weak var venuePlaceLabel: UILabel!
+    @IBOutlet weak var venueCommentLabel: UILabel!
+
+}
